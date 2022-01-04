e9d23ab47873f7345824a3757be1b5c4c7b4bed2
Md. Akramul Hoque <ictianlikhon6@gmail.com>
2022-01-04 17:26:46 +0600

    almost

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ async function run() {
         // console.log('Databse Connected');
         const database = client.db('CareerImpacts');
         const jobs = database.collection('jobs');
+        const appliedJobs = database.collection('Applied');
         const usersCollection = database.collection('Users');
 
 
@@ -51,12 +52,23 @@ async function run() {
             res.json(result);
         });
 
+
         // find a single job api
         app.get('/jobs/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await jobs.findOne(query);
             // console.log('Find with id', id);
+            res.send(result);
+        });
+
+        // get manage jobs api
+        app.get('/manageJobs', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const cursor = jobs.find(query);
+            const result = await cursor.toArray();
+            console.log(result);
             res.send(result);
         });
 
@@ -76,113 +88,53 @@ async function run() {
             res.json(result);
         })
 
+        // get specific job information api
+        app.get('/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await jobs.findOne(query);
+            // console.log('Find with id', id);
+            res.send(result);
+        });
 
-        // // get my myAttendance api
-        // app.get('/allAttendance', async (req, res) => {
-        //     const email = req.query.email;
-        //     const query = { email: email };
-        //     const cursor = recordTime.find(query);
-        //     const result = await cursor.toArray();
-        //     console.log(result);
-        //     res.send(result);
-        // });
+        // insert appliedJobs job api
+        app.post('/appliedJobs', async (req, res) => {
+            const job = req.body;
+            const result = await appliedJobs.insertOne(job);
+            console.log(result);
 
+            res.json(result);
+        });
 
-
-        // // get admin user
-        // app.get('/users/:email', async (req, res) => {
-        //     const email = req.params.email;
-        //     const query = { email: email };
-        //     const user = await usersCollection.findOne(query);
-        //     let isAdmin = false;
-        //     if (user?.role === 'Admin') {
-        //         isAdmin = true;
-        //     }
-        //     res.json({ admin: isAdmin })
-        // })
-
-
-        // //get all users api
-        // app.get("/members", async (req, res) => {
-        //     const result = await usersCollection.find({}).toArray();
-        //     // console.log(req.body);
-        //     res.send(result);
-        // });
-
-        // // get specific user information api
-        // app.get('/members/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) };
-        //     const result = await usersCollection.findOne(query);
-        //     // console.log('Find with id', id);
-        //     res.send(result);
-        // });
-
-        // // update specific users information api 
-        // app.put('/members/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const updateData = req.body;
-        //     const filter = { _id: ObjectId(id) };
-        //     const options = { upsert: true };
-        //     const updateDoc = {
-        //         $set: {
-        //             displayName: updateData.displayName,
-        //             email: updateData.email,
-        //             password: updateData.password,
-        //             role: updateData.role,
-        //             entryDate: updateData.entryDate,
-        //         },
-        //     };
-        //     const result = await usersCollection.updateOne(filter, updateDoc, options);
-
-        //     console.log(result);
-        //     // console.log(req.body);
-        //     res.json(result);
-        // })
-
-        // // Delete/remove specific users
-        // app.delete("/users/:id", async (req, res) => {
-        //     console.log(req.params.id);
-        //     const result = await usersCollection.deleteOne({
-        //         _id: ObjectId(req.params.id),
-        //     });
-        //     res.send(result);
-        // });
-
-        // // insert a user
-        // app.post('/users', async (req, res) => {
-        //     const user = req.body;
-        //     const result = await usersCollection.insertOne(user);
-        //     console.log(result);
-        //     res.json(result);
-        // });
+        // insert a user
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
 
 
-        // // update a user
-        // app.put('/users', async (req, res) => {
-        //     const user = req.body;
-        //     const filter = { email: user.email };
-        //     const options = { upsert: true };
-        //     const updateDoc = {
-        //         $set: {
-        //             user
-
-        //         }
-        //     };
-        //     const result = await usersCollection.updateOne(filter, updateDoc, options);
-        //     res.json(result);
-        // });
+        // get ceo api
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            console.log(user);
+            res.json(user);
+        })
 
 
-        // // update user role
-        // app.put('/users/admin', async (req, res) => {
-        //     const user = req.body;
-        //     const filter = { email: user.email };
-        //     const updateDoc = { $set: { role: 'Admin' } };
-        //     const result = await usersCollection.updateOne(filter, updateDoc);
-        //     console.log('put', result);
-        //     res.json(result);
-        // })
+        // get my myApplied api
+        app.get('/myApplied', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const cursor = appliedJobs.find(query);
+            const result = await cursor.toArray();
+            console.log(result);
+            res.send(result);
+        });
+
     }
     finally {
         // await client.close();
